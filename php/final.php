29c542ae00049a96e98a4e86688830f325320e3e
Grazi <diehlgraziela@gmail.com>
2022-09-01 09:42:04 -0300

    final.php

---
+++ php/final.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Senac Quiz</title>
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <header class="final">
+        <div class="container">
+            <h1>Quiz</h1>
+        </div>
+    </header>
+
+    <main class="final">
+        <div class="container">
+            <h2>Pontuação final: 5</h2>
+            <a href="question.php?n=1" class="start">Reiniciar</a>
+        </div>
+    </main>
+</body>
+
+</html>
