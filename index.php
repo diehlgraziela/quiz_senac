fef88cc17a2e94b89d2d592d653ab17572a64df3
Grazi <diehlgraziela@gmail.com>
2022-09-02 09:04:09 -0300

    dinamic number of questions

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php include 'php/connection.php'; ?>
 
+<?php
+//get total number of questions
+$query = "SELECT * FROM questions";
+
+//get results
+$results = $mysqli->query($query) or die($mysqli->error . __LINE__);
+$total = $results->num_rows;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,7 +29,7 @@
     <main class="first-page">
         <div class="container">
             <ul>
-                <li><b>Número de questões:</b> 5</li>
+                <li><b>Número de questões:</b> <?php echo $total; ?></li>
             </ul>
             <a href="php/question.php?n=1" class="start">Começar Quiz</a>
         </div>
